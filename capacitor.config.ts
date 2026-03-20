c1c1b1e417475fdf326308e7940a941f20e57517
nickflza <45699480+nickflza@users.noreply.github.com>
2026-03-20 08:56:03 +0100

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.monochrome.app',
-  appName: 'Fabiodalez Music',
+  appName: 'Monochrome',
   webDir: 'dist',
   plugins: {
     SystemBars: {
